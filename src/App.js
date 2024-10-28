6d532e828e139cf875fda471e44fd86e33a781e5
SrividyaBoddu724 <srividyasiri234@gmail.com>
2024-10-28 16:43:05 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const api = {
+  key: "d8ae9deabd0eb135eedf17b19232df55",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const searchPressed = () => {
+    if (!search) return; 
+    setLoading(true);
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("City not found");
+        }
+        return res.json();
+      })
+      .then((result) => {
+        setWeather(result);
+        setLoading(false);
+        setSearch(""); 
+      })
+      .catch((error) => {
+        alert(error.message);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Enter city/town..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+        {loading ? <p>Loading...</p> : null}
+        {typeof weather.main !== "undefined" ? (
+          <div>
+            <p>{weather.name}</p>
+            <p>{weather.main.temp}°C</p>
+            <p>{weather.weather[0].main}</p>
+            <p>({weather.weather[0].description})</p>
+          </div>
+        ) : (
+          ""
+        )}
       </header>
     </div>
   );
